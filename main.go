b9848f55b149c71b69c0eff9f94df4b364960fdb
Slaals <jonathan.serra@utt.fr>
2016-06-02 14:16:10 +0200

    STRUCT : unit test

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	{
 		v1.GET("/heartbeat", func(c *gin.Context) {
 			c.JSON(200, gin.H {
-				"message": "Hello world!",
+				"message": "Hellow world!",
 			})
 		})
 		v1.GET("/dom", ctrl.DomGET)
--- tests/main_test.go
+++ tests/main_test.go
@@ -3,18 +3,18 @@ package tests
 import (
   "testing"
   "github.com/gin-gonic/gin"
-  describe "github.com/smartystreets/goconvey/convey"
+  . "github.com/smartystreets/goconvey/convey"
   "net/http"
   "net/http/httptest"
 )
 
 func TestMain(t *testing.T) {
   router := gin.New()
-  router.GET("/v1/heartbeat", func(c *Context) {})
-  describe("Ask the server", t, func() {
+  router.GET("/v1/heartbeat", func(c *gin.Context) {})
+  Convey("Ask the server", t, func() {
     req, _ := http.NewRequest("GET", "/v1/heartbeat", nil)
-    resp := httptest.NewRecorder()
-    router.ServeHTTP(resp, req)
-    So(res.Code, ShouldEqual, http.StatusBadRequest)
+    res := httptest.NewRecorder()
+    router.ServeHTTP(res, req)
+    So(res.Code, ShouldEqual, http.StatusOK)
   })
 }
