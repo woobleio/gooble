3f9846b14473f95d06b2d3bd94239a03b5599f9c
Slaals <jonathan.serra@utt.fr>
2016-06-13 17:47:40 +0200

    FEATURE : get crea

--- app/v1/controllers/creation_controller.go
+++ app/v1/controllers/creation_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+  "net/http"
   "wobblapp/lib"
   m "wobblapp/app/v1/models"
   "github.com/gin-gonic/gin"
@@ -29,12 +30,38 @@ func CreationPOST(c *gin.Context) {
   styleID := bson.NewObjectId()
   styleC.Insert(&m.Style{ID: styleID, Style: style})
 
-  insertErr := creationC.Insert(&m.Creation{Title: title, DOM: domID, Style: styleID, Script: scriptID})
+  insertErr := creationC.Insert(&m.Creation{Title: title, Dom: domID, Style: styleID, Script: scriptID})
   if insertErr != nil {
     panic(insertErr)
   }
 }
 
 func CreationGET(c *gin.Context) {
+  // TODO populate function in lib & refactor Collections 
+  s := lib.GetSession()
+  creationC := s.DB("").C("Creation")
+  domC := s.DB("").C("DOM")
+  scriptC := s.DB("").C("Script")
+  styleC := s.DB("").C("Style")
+  defer s.Close()
 
+  title := c.Query("title")
+
+  creaResult := m.Creation{}
+  err := creationC.FindId(title).One(&creaResult)
+  if err != nil {
+    panic(err)
+  }
+  domRes := m.DOM{}
+  scriptRes := m.Script{}
+  styleRes := m.Style{}
+  domC.FindId(creaResult.Dom).One(&domRes)
+  scriptC.FindId(creaResult.Script).One(&scriptRes)
+  styleC.FindId(creaResult.Style).One(&styleRes)
+  c.JSON(http.StatusOK, gin.H{
+    "title": creaResult.Title,
+    "dom": domRes.Dom,
+    "script": scriptRes.Script,
+    "style": styleRes.Style,
+  })
 }
--- app/v1/models/creation_model.go
+++ app/v1/models/creation_model.go
@@ -4,7 +4,7 @@ import "gopkg.in/mgo.v2/bson"
 
 type Creation struct {
   Title string `bson:"_id"`
-  DOM bson.ObjectId
+  Dom bson.ObjectId
   Style bson.ObjectId
   Script bson.ObjectId
 }
