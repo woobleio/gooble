744102ce73bcb6d2a8f9fcb5f21c1fe176d6510b
Slaals <jonathan@wooble.io>
2017-04-24 20:36:26 +0100

    Fixes preview

--- router/handlers/creation.go
+++ router/handlers/creation.go
@@ -285,7 +285,7 @@ func SaveVersion(c *gin.Context) {
 	preview := `<html>
 		<head>
 			<script type="text/javascript">` + codeForm.Script + `</script>
-			<script type="text/javascript">(function(){woobly._init()})()</script>
+			<script type="text/javascript">window.onload = function(){woobly._init()}</script>
 			<style>` + codeForm.Style + `</style>
 		</head>
 		<body>
